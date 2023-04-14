afba799739ba7070a603590d0a771105404b0287
Sabir <bovba179@gmail.com>
2023-04-14 17:13:13 +0300

    second data

--- data.sql
+++ data.sql
@@ -1,30 +1,51 @@
 create schema homework;
 
-create table homework.PERSONS (
+create table homework.CUSTOMERS (
+    id SERIAL primary key,
     name varchar(30) NOT NULL,
     surname varchar (30) NOT NULL,
     age int CHECK (age>=0) CHECK (age<=100) NOT NULL,
-    phone_number varchar(11) UNIQUE,
-    city_of_living varchar(30),
-    PRIMARY KEY (name, surname, age)
+    phone_number varchar(11) UNIQUE
 );
 
 
-insert into homework.PERSONS (name, surname, age, phone_number, city_of_living)
-VALUES ('Vladimir', 'Max', 35, 89999998877, 'MOSCOW');
+insert into homework.CUSTOMERS (name, surname, age, phone_number)
+VALUES ('Vladimir', 'Max', 35, 89999998877);
 
-insert into homework.PERSONS (name, surname, age, phone_number, city_of_living)
-VALUES ('Ola', 'Min', 28, 89999998876, 'PITER');
+insert into homework.CUSTOMERS (name, surname, age, phone_number)
+VALUES ('Ola', 'Min', 28, 89999998876);
 
-insert into homework.PERSONS (name, surname, age, phone_number, city_of_living)
-VALUES ('Sabir', 'Bishop', 25, 89999998875, 'MOSCOW');
+insert into homework.CUSTOMERS (name, surname, age, phone_number)
+VALUES ('Sabir', 'Bishop', 25, 89999998875);
 
-
-SELECT name, surname
-FROM homework.PERSONS
-WHERE city_of_living = 'MOSCOW';
+insert into homework.CUSTOMERS (name, surname, age, phone_number)
+VALUES ('SABIR', 'Shop', 26, 89999998874);
 
 
-SELECT * FROM homework.PERSONS
-WHERE age>27
-ORDER BY age DESC;
+create table homework.ORDERS(
+    id SERIAL PRIMARY KEY ,
+    data date default current_date,
+    CustomerId INTEGER,
+    product_name varchar(30) NOT NULL,
+    amount int CHECK (amount>=0) not null,
+    FOREIGN KEY (CustomerId) REFERENCES homework.CUSTOMERS (id)
+);
+
+
+insert into homework.ORDERS(CustomerId, product_name, amount)
+VALUES (1, 'стириальная машинка', 5000);
+
+insert into homework.ORDERS(CustomerId, product_name, amount)
+VALUES (2, 'пылесос', 2500);
+
+insert into homework.ORDERS(CustomerId, product_name, amount)
+VALUES (3, 'часы', 1500);
+
+insert into homework.ORDERS(CustomerId, product_name, amount)
+VALUES (4, 'машина', 150000);
+
+SELECT product_name
+FROM homework.ORDERS
+INNER JOIN homework.CUSTOMERS
+ON orders.customerid = customers.id
+WHERE LOWER(name) = 'sabir';
